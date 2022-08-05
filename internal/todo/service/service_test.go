3dc0224ac6bdd982f6c1476972d6432179a1f645
Caio Aureliano <caio.aureliano1@gmail.com>
2022-08-04 22:56:16 -0300

    test: GetById with valid id and invalid id with error response

--- internal/todo/service/service_test.go
+++ internal/todo/service/service_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/CaioAureliano/go-do/internal/todo/model"
@@ -18,21 +19,67 @@ func TestCreate(t *testing.T) {
 }
 
 func TestGetById(t *testing.T) {
-	idMock := "abc1234"
+	tests := []struct {
+		name string
 
-	todoRepository = func() repository.TodoRepository {
-		return mockRepository{
-			fnGetById: func(id string) (*model.Todo, error) {
-				return &model.Todo{
-					ID: idMock,
-				}, nil
+		gotId          string
+		mockRepository repository.TodoRepository
+
+		wantError    assert.ErrorAssertionFunc
+		expectedErr  error
+		wantResponse *model.Todo
+	}{
+		{
+			name: "should be return a to-do",
+
+			gotId: "xyz1234",
+			mockRepository: mockRepository{
+				fnGetById: func(id string) (*model.Todo, error) {
+					return &model.Todo{
+						ID:     "xyz1234",
+						Task:   "tests to go-do",
+						Status: false,
+					}, nil
+				},
 			},
-		}
+
+			wantError:   assert.NoError,
+			expectedErr: nil,
+			wantResponse: &model.Todo{
+				ID:     "xyz1234",
+				Task:   "tests to go-do",
+				Status: false,
+			},
+		},
+		{
+			name: "should be return a error and nil response",
+
+			gotId: "abc123",
+			mockRepository: mockRepository{
+				fnGetById: func(id string) (*model.Todo, error) {
+					return nil, errors.New("repository error")
+				},
+			},
+
+			wantError:    assert.Error,
+			expectedErr:  ErrNotFoundTodo,
+			wantResponse: nil,
+		},
 	}
 
-	todoService := New()
-	todoFound, err := todoService.GetById(idMock)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 
-	assert.Equal(t, idMock, todoFound.ID)
-	assert.NoError(t, err)
+			todoRepository = func() repository.TodoRepository {
+				return tt.mockRepository
+			}
+
+			todoService := New()
+			res, err := todoService.GetById(tt.gotId)
+
+			tt.wantError(t, err)
+			assert.Equal(t, tt.expectedErr, err)
+			assert.Equal(t, tt.wantResponse, res)
+		})
+	}
 }
