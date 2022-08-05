da56ebc001ff5bfc7409360fd700b5e05008fccb
Caio Aureliano <caio.aureliano1@gmail.com>
2022-08-04 22:45:36 -0300

    feat: get item by id, if exists

--- internal/todo/service/service.go
+++ internal/todo/service/service.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"errors"
+	"log"
+
 	"github.com/CaioAureliano/go-do/internal/todo/model"
 	"github.com/CaioAureliano/go-do/internal/todo/repository"
 )
@@ -17,12 +20,22 @@ func New() TodoService {
 	return todoService{}
 }
 
-var todoRepository = repository.New
+var (
+	todoRepository = repository.New
+
+	ErrNotFoundTodo = errors.New("not found to-do")
+)
 
 func (t todoService) Create(task string) error {
 	return nil
 }
 
 func (t todoService) GetById(id string) (*model.Todo, error) {
-	return todoRepository().GetById(id)
+	todo, err := todoRepository().GetById(id)
+	if err != nil {
+		log.Printf("not found to-do with ID: %s [%s]", id, err.Error())
+		return nil, ErrNotFoundTodo
+	}
+
+	return todo, nil
 }
