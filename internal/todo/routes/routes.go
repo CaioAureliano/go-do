a12c6ec69ac857829da88ac7c185125276c9518b
Caio Aureliano <caio.aureliano1@gmail.com>
2022-10-12 16:09:26 -0300

    docs: setup swagger api documentation and add first endpoint to doc

--- internal/todo/routes/routes.go
+++ internal/todo/routes/routes.go
@@ -1,13 +1,17 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/CaioAureliano/go-do/internal/todo/handler"
+	"github.com/go-openapi/runtime/middleware"
 	"github.com/gorilla/mux"
 )
 
 func Router() *mux.Router {
 	r := mux.NewRouter()
 
+	docsHandler(r)
 	r.HandleFunc("/", handler.FindTodosHandler).Methods("GET")
 	r.HandleFunc("/", handler.CreateTodoHandler).Methods("POST")
 	r.HandleFunc("/{id}", handler.GetTodoByIdHandler).Methods("GET")
@@ -16,4 +20,13 @@ func Router() *mux.Router {
 	r.HandleFunc("/{id}/status", handler.UpdateTodoStatusByIdHandler).Methods("PATCH")
 
 	return r
+}
+
+func docsHandler(r *mux.Router) {
+	r.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
+
+	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
+	docsHandler := middleware.Redoc(opts, nil)
+
+	r.Handle("/docs", docsHandler)
 }
